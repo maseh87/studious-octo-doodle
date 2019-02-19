d6c6db45c74ad83f767e8a971f720f409a0731f4
Mason Hargrove <maseh87@gmail.com>
2019-02-19 16:37:51 -0500

    added updates

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+// Broadcast a message to connected users when someone connects or disconnects.
+// Add support for nicknames.
+// Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// Add “{user} is typing” functionality.
+// Show who’s online.
+// Add private messaging.
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
