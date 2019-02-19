adba039ca8a46f83f10cd5aa80d6755986394cd6
Mason Hargrove <maseh87@gmail.com>
2019-02-19 17:30:57 -0500

    notes

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ io.on('connection', function(socket) {
 
 
 
-
-
-
-
-
 http.listen(3000, function() {
   console.log('Listening on *:3k');
 });
---
+++ js/index.js
@@ -0,0 +1,32 @@
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+//
+//
+// // Add support for nicknames.
+// // Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// // Add “{user} is typing” functionality.
+// // Show who’s online.
+// // Add private messaging.
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// io.on('connection', function(socket) {
+//   console.log('a user connected');
+//   // Broadcast a message to connected users when someone connects or disconnects.
+//   socket.emit('connected', {username: 'Mason'});
+//
+//   socket.on('chat message', function(msg) {
+//     io.emit('chat message', msg);
+//   });
+//
+//
+// });
+//
+//
+//
+// http.listen(3000, function() {
+//   console.log('Listening on *:3k');
+// });
