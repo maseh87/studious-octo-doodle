817632803311350621111e893fa42932659c8b1a
Mason Hargrove <maseh87@gmail.com>
2019-02-19 17:39:04 -0500

    updating client

--- client/js/index.js
+++ client/js/index.js
@@ -1,32 +1,20 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+// <script>
+//   $(function () {
+//     var socket = io();
+//     $('form').submit(function(e){
+//       e.preventDefault(); // prevents page reloading
+//       socket.emit('chat message', $('#m').val());
+//       $('#m').val('');
+//       return false;
+//     });
 //
-//
-// // Add support for nicknames.
-// // Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-// // Add “{user} is typing” functionality.
-// // Show who’s online.
-// // Add private messaging.
-//
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// io.on('connection', function(socket) {
-//   console.log('a user connected');
-//   // Broadcast a message to connected users when someone connects or disconnects.
-//   socket.emit('connected', {username: 'Mason'});
-//
-//   socket.on('chat message', function(msg) {
-//     io.emit('chat message', msg);
+//     socket.on('chat message', function(msg){
+//       $('#messages').append($('<li>').text(msg));
+//     });
+//     // Broadcast a message to connected users when someone connects or disconnects.
+//     socket.on('connected', function(msg) {
+//       console.log('message: ', msg);
+//       $('#messages').append($('<li>').text(msg.username + ' has joined'));
+//     });
 //   });
-//
-//
-// });
-//
-//
-//
-// http.listen(3000, function() {
-//   console.log('Listening on *:3k');
-// });
+// </script>
