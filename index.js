e662f0340609e256fbce0d6164354442485d92a9
Mason Hargrove <maseh87@gmail.com>
2019-02-20 14:39:24 -0500

    constructing user object

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var _ = require('lodash');
 
+// Show who’s online.
 // Add support for nicknames.
 // Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-// Show who’s online.
 // Add private messaging.
+
 let userObj = {
   userName: 'guest',
   message: '',
@@ -17,20 +19,24 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket) {
 
-  // Broadcast a message to connected users when someone connects or disconnects.
-  socket.emit('connected', userObj);
-
-
+  // when the username is input
   socket.on('username', function(data) {
-    console.log('got username from frontend: ', data)
-    userObj.userName = data.userName;
+    // this is going to cause a problem
+    _.merge(userObj, data);
+
+    // userObj.userName = data.userName;
+
     // push the next user into the users storage
-    userObj.usersOnline.push(data.userName);
+    // userObj.usersOnline.push(data.userName);
+
+
     // send it back to the frontend
     socket.emit('username', userObj);
   });
+
   // listen for chat messages
   socket.on('chat message', function(msgObj) {
     console.log(msgObj);
@@ -38,11 +44,10 @@ io.on('connection', function(socket) {
   });
 
   // Add “{user} is typing” functionality.
-  
 
 
-
-
+  // Broadcast a message to connected users when someone connects or disconnects.
+  socket.emit('connected', userObj);
 
 });
 
