4b2e8468c2e5ef171aad23505b367005c0ea5080
Mason Hargrove <maseh87@gmail.com>
2019-02-20 11:53:04 -0500

    added feature

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var io = require('socket.io')(http);
 
 // Add support for nicknames.
 // Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-// Add “{user} is typing” functionality.
 // Show who’s online.
 // Add private messaging.
-let userObj = {};
+let userObj = {
+  userName: 'guest',
+  message: '',
+  usersOnline: []
+};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -23,18 +26,25 @@ io.on('connection', function(socket) {
   socket.on('username', function(data) {
     console.log('got username from frontend: ', data)
     userObj.userName = data.userName;
+    // push the next user into the users storage
+    userObj.usersOnline.push(data.userName);
+    // send it back to the frontend
     socket.emit('username', userObj);
   });
-
+  // listen for chat messages
   socket.on('chat message', function(msgObj) {
     console.log(msgObj);
     io.emit('chat message', msgObj);
   });
 
+  // Add “{user} is typing” functionality.
+  
+
+
+
+
 
 });
-
-
 
 http.listen(3000, function() {
   console.log('Listening on *:3k');
