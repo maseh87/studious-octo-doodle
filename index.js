43a05da0ab8f33ccd19ecf78b17500cff59ea9a3
Mason Hargrove <maseh87@gmail.com>
2019-02-20 15:11:16 -0500

    building socket handling

--- index.js
+++ index.js
@@ -15,6 +15,27 @@ let userObj = {
   usersOnline: []
 };
 
+// handler functions
+let joinHandler = function() {
+
+};
+
+let getChatroomsHandler = function() {
+
+};
+
+let leaveHandler = function() {
+
+};
+
+let disconnectHandler = function() {
+
+};
+
+let getOnlineUsers = function() {
+
+};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -31,7 +52,6 @@ io.on('connection', function(socket) {
 
     // push the next user into the users storage
     // userObj.usersOnline.push(data.userName);
-
 
     // send it back to the frontend
     socket.emit('username', userObj);
@@ -53,6 +73,24 @@ io.on('connection', function(socket) {
   // Broadcast a message to connected users when someone connects or disconnects.
   socket.emit('connected', userObj);
 
+  // user joins
+  socket.on('join', joinHandler);
+
+  // user leaves
+  socket.on('leave', leaveHandler);
+
+  // chatrooms
+  socket.on('chatrooms', getChatroomsHandler);
+
+  // get online users
+  socket.on('onlineUsers', getOnlineUsers)
+
+  // disconnection
+  socket.on('disconnect', function() {
+    console.log('User disconnected: ', socket.id);
+    // disconnectHandler();
+  });
+
   // Error handling
   socket.on('error', function(err) {
     console.log('recieved error from user: ', socket.id);
@@ -60,6 +98,7 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
+http.listen(3000, function(err) {
+  if (err) throw err
   console.log('Listening on *:3k');
 });
